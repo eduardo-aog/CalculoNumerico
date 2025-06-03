651352aa90bcd974fac4c294d65359d32d32525c
AleC2111 <AJcepedavelasco@gmail.com>
2025-06-03 10:39:12 -0400

    Multiplicación en hexadecimal y correcciones

--- Repositories/ElementalOperations.py
+++ Repositories/ElementalOperations.py
@@ -13,6 +13,8 @@ class ElementalOperations:
             raise AttributeError("Valor negativo con formato no permitido")
         if not self.__utilValFractionFormat(num):
             raise AttributeError("Valor de fracción con formato no permitido")
+        if not self.__utilValSpecialChar(num):
+            raise AttributeError("Valor no permitido no es un número")
         self.__num = num
 
     def __utilValOp(self, num):
@@ -28,10 +30,9 @@ class ElementalOperations:
         for i in num:
             if i not in "01":
                 return op
-            if self.__utilCheckSpecialChar(i):
-                return ""
         sum = self.__utilBinSum(num, num)
         minus = self.__utilBinMinus(num, num)
+        product = self.__utilBinMult(num, "10")
         
         return "+, -, *, /, ''+''"
 
@@ -39,10 +40,12 @@ class ElementalOperations:
         for i in num:
             if i not in "-,.0123456789":
                 return op
-            if self.__utilCheckSpecialChar(i):
-                return ""
         if not self.__utilDecSum(num, num):
             return ""   
+        if not self.__utilDecMinus(num, num):
+            return ""
+        if not self.__utilDecMult(num, "2"):
+            return ""
         
         return "+, -, *, /, ''+''"
 
@@ -50,19 +53,11 @@ class ElementalOperations:
         for i in num:
             if i.lower() not in "0123456789abcdef":
                 return op
-            if self.__utilCheckSpecialChar(i):
-                return ""
         sum = self.__utilHexSum(num, num)
         minus = self.__utilHexMinus(num, num)
-        product = self.__utilBinMult(num, "10")
+        product = self.__utilHexMult(num, "2")
 
         return "+, -, *, /, ''+''"
-    
-    def __utilCheckSpecialChar(n):
-        specialChars = "qwrtyuiopsghjklñzxvnm|°¬!#$%&/()=?¡'¿´+{}[];:_¨*"
-        if n.lower() in specialChars:
-            return True
-        return False
     
     def __utilXorGate(a, b):
         if (a or b) and not(a and b):
@@ -175,9 +170,7 @@ class ElementalOperations:
             else:
                 positionSum = self.__utilPositionToHex((positionNum1 + positionNum2))
                 carry = "0"
-
             sum = positionSum + sum
-            
         if carry=="1":
             sum = "1" + sum    
 
@@ -210,25 +203,65 @@ class ElementalOperations:
         return minus
     
     def __utilHexMult(self, num1, num2):
-        product = ""
-        mult1 = ""
-        mult2 = ""
+        aux = ""
+        carry = ""
+        product = "0"
+        mult = ""
         multiplierHex = ""
 
         for i in range(len(num2)):
-            lastToFirst = len(num2)-1-i
-            multiplierHex = self.__utilHexToPosition(num2[lastToFirst])
-            if multiplierHex>1:
-                for i in range(multiplierHex-1):
-                    mult1 = self.__utilHexSum(num1, num1) + mult1
-            else:
-                mult1 = "0" + mult1
-        
+            mult = ""
+            carry = ""
+            lastToFirst2 = len(num2)-1-i
+            for j in range(len(num1)):
+                lastToFirst1 = len(num1)-1-j
+                multiplierHex = self.__utilHexToPosition(num2[lastToFirst2])
+                if multiplierHex>1:
+                    for k in range(multiplierHex-1):
+                        aux = self.__utilHexSum(num1[lastToFirst1], num1[lastToFirst1])
+                    if carry!="":
+                        aux = self.__utilHexSum(aux, carry)                    
+                    mult = aux[len(aux)-1] + mult
+                    carry = aux[:-1]     
+                elif multiplierHex==1:
+                    if carry=="":
+                        mult = num1[lastToFirst1] + mult
+                    else:
+                        aux = self.__utilHexSum(num1[lastToFirst1], carry[len(carry)-1])                        
+                        mult = aux[len(aux)-1] + mult
+                        carry = aux[:-1]                        
+                else:
+                    if carry=="":
+                        mult = "0" + mult
+                    else:
+                        mult = carry[len(carry)-1] + mult
+                        carry = carry[:-1]
+                if j==len(num1)-1 and carry!="":
+                    mult = carry + mult         
+            if i!=0:
+                for m in range(i):
+                    mult = mult + "0"
+            product = self.__utilHexSum(mult, product)
+
         return product
 
     
     def __utilDecSum(num1, num2): #Puede arrojar ValueError
         test = str(float(num1) + float(num2)) 
+        for i in test:
+            if i not in "-,.0123456789":
+                return False
+        return True
+    
+    def __utilDecMinus(num1, num2):
+        test = str(float(num1) - float(num2)) 
+        for i in test:
+            if i not in "-,.0123456789":
+                return False
+        return True
+    
+    def __utilDecMult(num1, num2):
+        test = str(float(num1) * float(num2)) 
         for i in test:
             if i not in "-,.0123456789":
                 return False
@@ -259,6 +292,13 @@ class ElementalOperations:
                 return True
             n += 1
         return True   
+    
+    def __utilValSpecialChar(num):
+        specialChars = "qwrtyuiopsghjklñzxvnm|°¬!#$%&/()=?¡'¿´+{}[];:_¨*"
+        for i in num:
+            if i.lower() in specialChars:
+                return False
+        return True
 
     def getNum(self):
         return self.__num
