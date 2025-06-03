3019945783e54bbde36c527e79db6baf90727076
Leandert23 <toroleander23@gmail.com>
2025-06-03 16:20:21 -0400

    Subida del objeto como función (sin horas ni minutos)

--- Storage/Archivo.py
+++ Storage/Archivo.py
@@ -1,17 +1,11 @@
 import random as rm
 import time as tm
-class Archivo:
-    def __init__(self):
-        self.serial1 = ""
-        self.fecha = ""
-        self.serial2 = ""
-        self.nombreArchivo()
         
-    def nombreArchivo(self):
-        self.serial1 = str(rm.randint(10,99))
-        self.fecha = tm.strftime("%Y/%m/%d_%H:%M") 
-        self.serial2 = str(rm.randint(10,99))
+def nombreArchivo():
+    serial1 = str(rm.randint(10,99))
+    fecha = tm.strftime("%Y/%m/%d") 
+    serial2 = str(rm.randint(10,99))
+    return serial1 + "_" + fecha + "_" + serial2
+
+    
         
-        
-Obj = Archivo()
-print(Obj.serial1 + "_" + Obj.fecha + "_" + Obj.serial2)
