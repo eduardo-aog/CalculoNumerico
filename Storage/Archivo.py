4172f6a95327223db126af40d903b34013d4cac0
Leandert23 <toroleander23@gmail.com>
2025-06-04 15:56:07 -0400

    Archivo.log

--- Storage/Archivo.py
+++ Storage/Archivo.py
@@ -3,9 +3,15 @@ import time as tm
         
 def nombreArchivo():
     serial1 = str(rm.randint(10,99))
-    fecha = tm.strftime("%Y/%m/%d") 
+    fecha = tm.strftime("%Y/%m/%d_%H:%M")
     serial2 = str(rm.randint(10,99))
     return serial1 + "_" + fecha + "_" + serial2
 
-    
-        
+def archivo_log(error):
+    with open("Archivo.log", "a", encoding = "utf-8") as archivo:
+        archivo.write(f"!!Error!!: {validarError(error)} ({tm.strftime("%Y/%m/%d_%H:%M")})\n")
+
+def validarError(error):
+    if type(error) != str:
+        archivo_log("Tipo de dato incorrecto")
+    return error
