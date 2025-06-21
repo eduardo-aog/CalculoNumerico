ecd4459350c72567fadc1df88d5a1ea3981fb425
eduardo-aog <eduardoaorozcog@gmail.com>
2025-06-20 20:45:34 -0400

    Prueba push, eliminacion de Main, Consult

--- Helpers/ConsultMain.py
+++
@@ -1,5 +0,0 @@
-from .ConsultData import showCase #$$$4U
-
-def ConsultMain(measuredValue, realValue, absoluteError, relativeError, roundError, truncError, propError):
-    showCase(measuredValue, realValue, absoluteError, relativeError, roundError, truncError, propError)
-    
--- Main.py
+++
@@ -1,9 +0,0 @@
-from Process.ProcessMain import ProcessMain
-from Helpers.ConsultMain import ConsultMain
-"""def Main():
-    measured = 9.5
-    real = 10.67
-    measuredValue, realValue, absoluteError, relativeError, roundError, truncError, propError = ProcessMain(measured, real)    
-    ConsultMain(measuredValue, realValue, absoluteError, relativeError, roundError, truncError, propError)
-    print("Fin del programa")"""
-# A partir de aquí, Gauss-Jordan y Seidel
