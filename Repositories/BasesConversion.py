79ced28172822bcb38881c8c876dd56a63e9c528
AlejandroC305C1 <AJcepedavelasco@gmail.com>
2025-07-02 12:39:43 -0400

    Refactorización: Organización de Bases Numéricas y Conversión, cambio de nombres de funciones y variables

--- Repositories/BasesConversion.py
+++ Repositories/BasesConversion.py
@@ -5,33 +5,31 @@ class BasesConversion:
     __numBase10 = ""
     __numBase16 = ""
 
-    def __init__(self, num, bases):
+    def __init__(self, num:str, bases:str):
         self.__utilValNum(num)
         self.__utilValBases(bases)
-        self.__utilValConversion()
+        self.__utilMenuConversionDecimal()
 
-    def __utilValNum(self, num):
+    def __utilValNum(self, num:str):
         if num == None:
             raise ValueError("Valor nulo no permitido")
         if not self.__utilValNegativeFormat(num):
             raise ValueError("Valor negativo con formato no permitido")
-        if "," in num:
-            num = self.__utilReplaceCommaFraction(num)
+        num = self.__utilReplaceCommaFraction(num)
         if not self.__utilValFractionFormat(num):
             raise ValueError("Valor de fracción con formato no permitido")
         if not self.__utilValSpecialChar(num):
             raise ValueError("Valor no permitido, no es un número")
         self.__num = num
 
-    def __utilValBases(self, bases):
+    def __utilValBases(self, bases:str):
         if bases == None:
             raise ValueError("Valor nulo no permitido")
         if not self.__utilValFormatBases(bases):
             raise ValueError("Formato no permitido para las bases")
-        
         self.__bases = bases
 
-    def __utilValConversion(self):
+    def __utilMenuConversionDecimal(self):
         separateBases = self.__bases.split("/")
         if "Binario"==separateBases[0]:
             self.__numBase2 = self.__utilBinToDecimal(self.__num)
@@ -50,7 +48,7 @@ class BasesConversion:
             self.__numBase10 = self.__num
             self.__numBase16 = "No puede ser tomado como Hexadecimal"        
         
-    def __utilBinToDecimal(self, num):
+    def __utilBinToDecimal(self, num:str) -> str:
         dec = 0
         for i in range(len(num)):
             lastToFirst = len(num)-1-i
@@ -58,7 +56,7 @@ class BasesConversion:
                 dec = str(2**i) + dec
         return dec
 
-    def __utilHexToDecimal(self, num):
+    def __utilHexToDecimal(self, num:str) -> str:
         hex = "0123456789abcdef"
         decimal = ""
         for i in range(len(num)):
@@ -68,12 +66,14 @@ class BasesConversion:
                     decimal = str(j*16**i) + decimal
         return decimal
 
-    def __utilValFormatBases(self, bases):
+    def __utilValFormatBases(self, bases:str) -> bool:
         if "/" not in bases:
+            return False
+        if "Binario" not in bases and "Decimal" not in bases and "Hexadecimal" not in bases:
             return False
         return True
 
-    def __utilValNegativeFormat(self, num):
+    def __utilValNegativeFormat(self, num:str) -> bool:
         n = 0
         for i in num:
             if "-" in num and (n == 0 and i!= "-"):
@@ -83,7 +83,7 @@ class BasesConversion:
             n += 1
         return True
 
-    def __utilValFractionFormat(self, num):
+    def __utilValFractionFormat(self, num:str) -> bool:
         n = 0
         for i in num:
             if "." in num and (n == 0 and i == "."):
@@ -95,16 +95,18 @@ class BasesConversion:
             n += 1
         return True
     
-    def __utilValSpecialChar(self, num):
+    def __utilValSpecialChar(self, num:str) -> bool:
         specialChars = "qwrtyuiopsghjklñzxvnm|°¬!#$%&/()=?¡'¿´+{}[];:_¨* "
         for i in num:
             if i.lower() in specialChars:
                 return False
         return True
     
-    def __utilReplaceCommaFraction(self, digit):
-        commaless = digit.split(",")
-        return commaless[0]+"."+commaless[1]
+    def __utilReplaceCommaFraction(self, num:str) -> str:
+        if "," in num:
+            commaless = num.split(",")
+            return commaless[0]+"."+commaless[1]
+        return num
 
     def getNum(self):
         return self.__num
@@ -112,11 +114,11 @@ class BasesConversion:
     def getBases(self):
         return self.__bases
     
-    def getNumPossiblyBin(self):
+    def getNumBinInDec(self):
         return self.__numBase2
     
-    def getNumPossiblyDec(self):
+    def getNumDec(self):
         return self.__numBase10
     
-    def getNumPossiblyHex(self):
+    def getNumHexInDec(self):
         return self.__numBase16
--- Repositories/NumBases.py
+++ Repositories/NumBases.py
@@ -2,51 +2,55 @@ class NumBases:
     __num = ""
     __base = ""
 
-    def __init__(self, num):
+    def __init__(self, num:str):
         self.__utilValNum(num)
-        self.__utilValBase(num)
+        self.__utilBases(num)
 
-    def __utilValNum(self, num):
+    def __utilValNum(self, num:str):
         if num == None:
             raise ValueError("Valor nulo no permitido")
         if not self.__utilValNegativeFormat(num):
             raise ValueError("Valor negativo con formato no permitido")
-        if "," in num:
-            num = self.__utilReplaceCommaFraction(num)
+        num = self.__utilReplaceCommaFraction(num)
         if not self.__utilValFractionFormat(num):
             raise ValueError("Valor de fracción con formato no permitido")
         if not self.__utilValSpecialChar(num):
             raise ValueError("Valor no permitido, no es un número")
         self.__num = num
 
-    def __utilValBase(self, num):
+    def __utilBases(self, num:str):
         base = ""
-        base = self.__utilValHex(num, base)
-        base = self.__utilValDec(num, base)
-        base = self.__utilValBin(num, base)
+        base = self.__utilHexInNumber(num, base)
+        base = self.__utilDecInNumber(num, base)
+        base = self.__utilBinInNumber(num, base)
         if base == "":
             raise ValueError("Valor no permitido, no es un número")
         self.__base = base
 
-    def __utilValBin(self, num, base):
-        for i in num:
-            if i not in "01":
-                return base
+    def __valNumsAllowed(numStr:str, stringValuesAllowed:str) -> bool:
+        if numStr==None:
+            return False
+        for i in numStr:
+            if i not in stringValuesAllowed:
+                return False
+        return True
+
+    def __utilBinInNumber(self, num:str, base:str) -> str:
+        if not self.__valNumsAllowed(num, "01"):
+            return base
         return "Binario/"+base
 
-    def __utilValDec(self, num, base):
-        for i in num:
-            if i not in "-,.0123456789":
-                return base
+    def __utilDecInNumber(self, num:str, base:str) -> str:
+        if not self.__valNumsAllowed(num, "-.0123456789"):
+            return base
         return "Decimal/"+base
 
-    def __utilValHex(self, num, base):
-        for i in num:
-            if i not in "0123456789ABCDEFabcdef":
-                return base
+    def __utilHexInNumber(self, num:str, base:str) -> str:
+        if not self.__valNumsAllowed(num, "0123456789ABCDEFabcdef"):
+            return base
         return "Hexadecimal/"+base
 
-    def __utilValNegativeFormat(self, num):
+    def __utilValNegativeFormat(self, num:str) -> bool:
         n = 0
         for i in num:
             if "-" in num and (n == 0 and i!= "-"):
@@ -56,7 +60,7 @@ class NumBases:
             n += 1
         return True
 
-    def __utilValFractionFormat(self, num):
+    def __utilValFractionFormat(self, num:str) -> bool:
         n = 0
         for i in num:
             if "." in num and (n == 0 and i == "."):
@@ -68,16 +72,18 @@ class NumBases:
             n += 1
         return True
     
-    def __utilValSpecialChar(self, num):
+    def __utilValSpecialChar(self, num:str) -> bool:
         specialChars = "qwrtyuiopsghjklñzxvnm|°¬!#$%&/()=?¡'¿´+{}[];:_¨* "
         for i in num:
             if i.lower() in specialChars:
                 return False
         return True
     
-    def __utilReplaceCommaFraction(self, digit):
-        commaless = digit.split(",")
-        return commaless[0]+"."+commaless[1]
+    def __utilReplaceCommaFraction(self, num:str) -> str:
+        if "," in num:
+            commaless = num.split(",")
+            return commaless[0]+"."+commaless[1]
+        return num
 
     def getNum(self):
         return self.__num
