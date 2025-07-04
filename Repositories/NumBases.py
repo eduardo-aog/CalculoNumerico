7d71135edcd2adb812ead59094440185a205501e
AlejandroC305C1 <AJcepedavelasco@gmail.com>
2025-07-03 23:35:59 -0400

    Refactorización: Cambio del nombre de variables para facilitar la lectura del código

--- Repositories/NumBases.py
+++ Repositories/NumBases.py
@@ -1,92 +1,92 @@
 class NumBases:
-    __num = ""
-    __base = ""
+    __numberToAnalize = ""
+    __basesAvailables = ""
 
-    def __init__(self, num:str):
-        self.__utilValNum(num)
-        self.__utilBases(num)
+    def __init__(self, numberToAnalize:str):
+        self.__utilValNum(numberToAnalize)
+        self.__utilBases(numberToAnalize)
 
-    def __utilValNum(self, num:str):
-        if num == None:
+    def __utilValNum(self, numberToAnalize:str):
+        if numberToAnalize == None:
             raise ValueError("Valor nulo no permitido")
-        if not self.__utilValNegativeFormat(num):
+        if not self.__utilValNegativeFormat(numberToAnalize):
             raise ValueError("Valor negativo con formato no permitido")
-        num = self.__utilReplaceCommaFraction(num)
-        if not self.__utilValFractionFormat(num):
+        numberToAnalize = self.__utilReplaceCommaFraction(numberToAnalize)
+        if not self.__utilValFractionFormat(numberToAnalize):
             raise ValueError("Valor de fracción con formato no permitido")
-        if not self.__utilValSpecialChar(num):
+        if not self.__utilValSpecialChar(numberToAnalize):
             raise ValueError("Valor no permitido, no es un número")
-        self.__num = num
+        self.__numberToAnalize = numberToAnalize
 
-    def __utilBases(self, num:str):
-        base = ""
-        base = self.__utilHexInNumber(num, base)
-        base = self.__utilDecInNumber(num, base)
-        base = self.__utilBinInNumber(num, base)
-        if base == "":
+    def __utilBases(self, numberToAnalize:str):
+        basesAvailables = ""
+        basesAvailables = self.__utilHexInNumber(numberToAnalize, basesAvailables)
+        basesAvailables = self.__utilDecInNumber(numberToAnalize, basesAvailables)
+        basesAvailables = self.__utilBinInNumber(numberToAnalize, basesAvailables)
+        if basesAvailables == "":
             raise ValueError("Valor no permitido, no es un número")
-        self.__base = base
+        self.__basesAvailables = basesAvailables
 
     def __valNumsAllowed(numStr:str, stringValuesAllowed:str) -> bool:
         if numStr==None:
-            return False
+            raise ValueError("Valor nulo no permitido")
         for i in numStr:
             if i not in stringValuesAllowed:
                 return False
         return True
 
-    def __utilBinInNumber(self, num:str, base:str) -> str:
-        if not self.__valNumsAllowed(num, "01"):
+    def __utilBinInNumber(self, numberToAnalize:str, base:str) -> str:
+        if not self.__valNumsAllowed(numberToAnalize, "01"):
             return base
         return "Binario/"+base
 
-    def __utilDecInNumber(self, num:str, base:str) -> str:
-        if not self.__valNumsAllowed(num, "-.0123456789"):
+    def __utilDecInNumber(self, numberToAnalize:str, base:str) -> str:
+        if not self.__valNumsAllowed(numberToAnalize, "-.0123456789"):
             return base
         return "Decimal/"+base
 
-    def __utilHexInNumber(self, num:str, base:str) -> str:
-        if not self.__valNumsAllowed(num, "0123456789ABCDEFabcdef"):
+    def __utilHexInNumber(self, numberToAnalize:str, base:str) -> str:
+        if not self.__valNumsAllowed(numberToAnalize, "0123456789ABCDEFabcdef"):
             return base
         return "Hexadecimal/"+base
 
-    def __utilValNegativeFormat(self, num:str) -> bool:
+    def __utilValNegativeFormat(self, numberToAnalize:str) -> bool:
         n = 0
-        for i in num:
-            if "-" in num and (n == 0 and i!= "-"):
+        for i in numberToAnalize:
+            if "-" in numberToAnalize and (n == 0 and i!= "-"):
                 return False
             if i == "-":
                 return True
             n += 1
         return True
 
-    def __utilValFractionFormat(self, num:str) -> bool:
+    def __utilValFractionFormat(self, numberToAnalize:str) -> bool:
         n = 0
-        for i in num:
-            if "." in num and (n == 0 and i == "."):
+        for i in numberToAnalize:
+            if "." in numberToAnalize and (n == 0 and i == "."):
                 return False
-            if ("." in num and "-" in num) and (n == 1 and i == "."):
+            if ("." in numberToAnalize and "-" in numberToAnalize) and (n == 1 and i == "."):
                 return False
             if i == ".":
                 return True
             n += 1
         return True
     
-    def __utilValSpecialChar(self, num:str) -> bool:
+    def __utilValSpecialChar(self, numberToAnalize:str) -> bool:
         specialChars = "qwrtyuiopsghjklñzxvnm|°¬!#$%&/()=?¡'¿´+{}[];:_¨* "
-        for i in num:
+        for i in numberToAnalize:
             if i.lower() in specialChars:
                 return False
         return True
     
-    def __utilReplaceCommaFraction(self, num:str) -> str:
-        if "," in num:
-            commaless = num.split(",")
+    def __utilReplaceCommaFraction(self, numberToAnalize:str) -> str:
+        if "," in numberToAnalize:
+            commaless = numberToAnalize.split(",")
             return commaless[0]+"."+commaless[1]
-        return num
+        return numberToAnalize
 
     def getNum(self):
-        return self.__num
+        return self.__numberToAnalize
 
     def getBase(self):
-        return self.__base
+        return self.__basesAvailables
